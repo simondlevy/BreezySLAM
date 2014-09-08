09076b51437a4b31da2a6e3f9b8f40fbf9fa7219
Simon D. Levy <simon.d.levy@gmail.com>
2014-09-07 21:11:34 -0400

    Create __init__.py

---
+++ python/breezyslam/__init__.py
@@ -0,0 +1,24 @@
+'''
+BreezySLAM: Simple, efficient SLAM in Python
+
+Copyright (C) 2014 Simon D. Levy
+
+This code is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 3 of the 
+License, or (at your option) any later version.
+
+This code is distributed in the hope that it will be useful,     
+but WITHOUT ANY WARRANTY without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License 
+along with this code.  If not, see <http:#www.gnu.org/licenses/>.
+You should also have received a copy of the Parrot Parrot AR.Drone 
+Development License and Parrot AR.Drone copyright notice and disclaimer 
+and If not, see 
+<https:#projects.ardrone.org/attafchments/277/ParrotLicense.txt> 
+and
+<https:#projects.ardrone.org/attachments/278/ParrotCopyrightAndDisclaimer.txt>.
+'''
